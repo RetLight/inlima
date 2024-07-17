c405f1264bf79ab97688b8812d200b048a7b1dc6
RLJ17 <20201192@aloe.ulima.edu.pe>
2024-07-17 01:23:33 -0500

    Fix

--- src/app/resultados/page.jsx
+++ src/app/resultados/page.jsx
@@ -1,11 +1,11 @@
 "use client";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Suspense } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import StatusCard from "@/components/StatusCard";
 import Layout from '@/components/Layout';
 import api from '@/api/queja'
 
-export default function ResultadosPage() {
+function ResultadosPage() {
 
     const router = useRouter();
     const searchParams = useSearchParams();
